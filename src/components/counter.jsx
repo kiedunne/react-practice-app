9a31af334ed2e35e0f3283327e85f2307e97eb88
kiedunne <kie.dunne@gmail.com>
2019-02-22 15:56:05 +0000

    increment button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,20 +5,31 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = {
     count: 0,
-
+    imageUrl: 'https://picsum.photos/200',
+    tags: ['tag1', 'tag2', 'tag3']
   };
 
-
-
-
+handleIncrement = () => {
+  console.log("Increment clicked", this);
+};
   render() {
     React.createElement('div')
     return (
     <React.Fragment>
-      <span>{this.formatCount()}</span>
-      <button>Increment</button>
+
+      <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+      <button onClick={this.handleIncrement}className='btn btn-secondary btn-nm'>Increment</button>
+      <ul>
+        { this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }
+      </ul>
     </React.Fragment>
    );
+  }
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-'
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes
   }
 
   formatCount() {
